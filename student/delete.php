14d57ae438b42e14cf79d6eb65b6a2c7f03c3868
seozudamin <karlarnoldcorrea24@gmail.com>
2024-11-15 23:16:07 +0800

    dashboard, delete, edit, register.php guard done

--- student/delete.php
+++ student/delete.php
@@ -1,5 +1,10 @@
 <?php
 session_start();
+if (!isset($_SESSION['email'])) {
+    // If the user is not logged in, redirect to the login page
+    header("Location: index.php");
+    exit();
+}
 
 // Initialize variables
 $studentId = '';
